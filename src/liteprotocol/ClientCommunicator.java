4fd4f289350d06414a729b48656b0a39f4edb043
Qian <qianz424@gmail.com>
2014-05-02 02:14:02 -0500

    Removed unused imports

--- src/liteprotocol/ClientCommunicator.java
+++ src/liteprotocol/ClientCommunicator.java
@@ -16,7 +16,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import liteprotocol.interfaces.Client;
